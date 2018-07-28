e0a977f0e767a78b20e129f53db684a20caf2a56
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-07-28 11:37:04 -0400

    backup.sh: Set finer exclusions
     - Exclude google-generated home dir
    
     - Exclude generated artifact directories
    
    Change-Id: Id6dc10111437f880e3a982fdd674d70095c5a38d

--- backup.sh
+++ backup.sh
@@ -12,9 +12,17 @@ LIST="
 "
 
 EXCLUDE="
-download.msm8916.com/public_html
-download.msm8953.com/public_html
 builds/full
+zvikovincent
+*.zip
+AOSPA
+dotOS
+DotOS
+LineageOS
+LineageOS_Go
+OpenGApps
+ResurrectionRemix
+TWRP
 .vim
 .cache
 .local
