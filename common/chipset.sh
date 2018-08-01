3a2a9b7279a91c0a8c0a86eb7e978d93e8ce57ad
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-01 07:26:02 -0400

    Change arch -> chipset
     - Misnomer
    
    Change-Id: I106a7f29bca291d7f6b25e7a9be4c88d45e6edfa

--- common/chipset.sh
+++ common/chipset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-arch=
+chipset=
 
 ARCH_DEVICES_FULL=
 ARCH_COMPACT_DEVICE_LIST=
@@ -17,34 +17,34 @@ function generate_device_list() {
     [ -z "$ARCH_DEVICES_FULL" ] && ARCH_DEVICES_FULL=`find $script_dir -name '*txt' | xargs grep DEVICES`
 
     for i in $ARCH_DEVICES_FULL; do
-	    arch=`echo $i | grep -o -e 'msm[0-9a-zA-Z]*'`;
-	    [ -z "$arch" ] && arch=`echo $i | grep -o -e 'exynos[0-9a-zA-Z]*'`;
+	    chipset=`echo $i | grep -o -e 'msm[0-9a-zA-Z]*'`;
+	    [ -z "$chipset" ] && chipset=`echo $i | grep -o -e 'exynos[0-9a-zA-Z]*'`;
 	    local device_name=`echo $i| cut -d'=' -f 2| cut -d':' -f 1`
 	    searched_device_name="`echo $ARCH_COMPACT_DEVICE_LIST | grep -o $device_name | uniq`" 
 	    if [ "x$searched_device_name" != "x$device_name" ]; then
-	        ARCH_COMPACT_DEVICE_LIST+="$device_name:$arch"
+	        ARCH_COMPACT_DEVICE_LIST+="$device_name:$chipset"
 	        ARCH_COMPACT_DEVICE_LIST+=$newline
             fi
     done
     ARCH_COMPACT_DEVICE_LIST=`echo $ARCH_COMPACT_DEVICE_LIST | sort | uniq`
 }
 
-function find_arch() {
+function find_chipset() {
 
     local device_name=$1
 
     [ -z "$ARCH_COMPACT_DEVICE_LIST" ] && generate_device_list
 
-    arch=`echo $ARCH_COMPACT_DEVICE_LIST|grep -o ${device_name}:'[a-zA-Z0-9]*'|uniq|cut -d':' -f2`
+    chipset=`echo $ARCH_COMPACT_DEVICE_LIST|grep -o ${device_name}:'[a-zA-Z0-9]*'|uniq|cut -d':' -f2`
 
     # default to msm8916
-    [ -z "${arch}" ] && arch=msm8916
-    [ -z "${device_name}" ] && arch=msm8916
+    [ -z "${chipset}" ] && chipset=msm8916
+    [ -z "${device_name}" ] && chipset=msm8916
 
-    echo $arch
+    echo $chipset
 }
 
-function print_arch_map() {
+function print_chipset_map() {
     [ -z "$ARCH_COMPACT_DEVICE_LIST" ] && generate_device_list
 
     (>&2 echo -e "Arch map is: $ARCH_COMPACT_DEVICE_LIST\n")
