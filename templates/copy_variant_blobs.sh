7d2a3f447acc2905c7f261c437c300e21b4e8357
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-08-29 12:44:45 -0400

    build: Simplify boot image zip generation
    Change-Id: I89dbdad17711c69431c2220fddfaa09715e4b3bf

--- templates/copy_variant_blobs.sh
+++ templates/copy_variant_blobs.sh
@@ -1,6 +1,8 @@
 #!/sbin/sh
 
 BLOBBASE=/tmp/proprietary
+IMG_BASE=/tmp/img
+BLOCKDEV_PATH=/dev/block/bootdevice/by-name
 
 # Mount /system
 mount_fs system
@@ -21,6 +23,23 @@ if [ -d $BLOBBASE ]; then
         ui_print "Setting /system/$FILE executable ..."
         chmod 755 /system/$FILE
     done
+
 umount_fs system
+
 fi
 
+if [ -d $IMG_BASE ]; then
+
+    for img in `find $IMG_BASE -type f | cut -c 3-` ; do
+        ui_print "Flashing `basename ${img}`..."
+	img_proper=`basename $img|sed s'/\.img//'g`
+	if [ -e ${BLOCKDEV_PATH}/${img_proper} ]; then
+            dd if=${img} of=${BLOCKDEV_PATH}/${img_proper}
+            ui_print "Wrote ${img_proper} succesfully."
+        else
+            ui_print "Error: device ${BLOCKDEV_PATH}/${img_proper} does not exist."
+            ui_print "Flashing ${img} failed!"
+	    exit 1
+	fi
+    done
+fi
